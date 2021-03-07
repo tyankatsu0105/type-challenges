a2cd370a54cad8c84bf1c37833543e874e325505
tyankatsu <frips.ryilsufupe+dev@gmail.com>
2021-03-07 19:05:28 +0900

    3

--- src/medium/3-medium-omit/index.ts
+++ src/medium/3-medium-omit/index.ts
@@ -30,15 +30,49 @@
 
 /* _____________ Your Code Here _____________ */
 
+/**
+ * 1. TのkeyからKのunionとマッチしたものを省いたobjectを返したい
+ * type MyOmit<T, K> = any
+ *
+ * 2. KはTのkeyと同じ文字列でないといけないので、keyof T
+ * type MyOmit<T, K extends T> = any
+ *
+ * 3. TのkeyからKのunionとマッチしたものを省いたobjectを返したいので、objectへ
+ * type MyOmit<T, K extends T> = {
+ *
+ * }
+ *
+ * 4. keyはTのkeyofのはずなので、extends keyof Tで、valueはTのkeyのvalueと同じ型のはずなのでT[key]
+ * type MyOmit<T, K extends T> = {
+ *  [key in keyof T]: T[key]
+ * }
+ *
+ * 5. keyから省くものを一つずつ走査するため、inferで抜き出し（ただしここの時点でTSはRをunknownとしてしまっているが一旦放置）、走査できない場合はnever
+ * type MyOmit<T, K extends T> = {
+ *  [key in keyof T extends infer R ? R : never]: T[key]
+ * }
+ *
+ * 6. Rが、省きたいkey名を列挙したKとマッチしたら消滅させたいので、extends Kでtrueならnever、falseならR
+ * type MyOmit<T, K extends T> = {
+ *  [key in keyof T extends infer R ? R extends K ? never : R : never]: T[key]
+ * }
+ *
+ * 7. ここから、Rはunknownであるので、Tのkey名の一部であることを教えてあげる必要がある。extends keyof T　違ったらnever
+ * type MyOmit<T, K extends T> = {
+ *  [key in keyof T extends infer R ? R extends K ? never : R extends keyof T ? R : never : never]: T[key]
+ * }
+ */
+
 type MyOmit<T, K extends keyof T> = {
-  [key in keyof T extends infer U
-    ? U extends K
+  [key in keyof T extends infer R
+    ? R extends K
       ? never
-      : U extends keyof T
-      ? U
+      : R extends keyof T
+      ? R
       : never
     : never]: T[key];
 };
+
 // type MyOmit<T, K extends keyof T> = {
 //   [P in Exclude<keyof T, K>]: T[P];
 // };
